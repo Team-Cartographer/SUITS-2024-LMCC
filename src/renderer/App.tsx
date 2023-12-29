4fbe2f6588a2b0638cd5de9ef848df017d3d8aed
abhi-arya1 <abhia9167@gmail.com>
2023-12-28 21:39:24 -0800

    add warnings and set proper window height

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -5,7 +5,7 @@ import { GitHub } from '@mui/icons-material';
 function Hello() {
   return (
     <div className="flex flex-col items-center justify-center default-font">
-      <h1 className="pt-5 font-extrabold text-6xl">Hello Tailwind</h1>
+      <h1 className="pt-5 font-extrabold text-7xl">Hello Tailwind</h1>
       <GitHub />
     </div>
   );
