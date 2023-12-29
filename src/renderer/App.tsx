4167a82c6934ad489ada246a9a91d00372681583
abhi-arya1 <abhia9167@gmail.com>
2023-12-28 23:36:45 -0800

    standardize indent size to 4

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -4,27 +4,27 @@ import Clock from '../components/clock';
 import teamCartographerLogo from '../../assets/icons/team_cartographer_logo_rounded_edges.png';
 
 function Hello() {
-  return (
-    <div className="flex flex-col items-left pl-2 justify-center default-font">
-      <img
-        src={teamCartographerLogo}
-        alt="Logo"
-        height={50}
-        width={50}
-        className="pt-5"
-      />
-      <h1 className="pt-5 font-extrabold text-7xl">LMCC Console</h1>
-      <Clock />
-    </div>
-  );
+    return (
+        <div className="flex flex-col items-left pl-2 justify-center default-font">
+            <img
+                src={teamCartographerLogo}
+                alt="Logo"
+                height={50}
+                width={50}
+                className="pt-5"
+            />
+            <h1 className="pt-5 font-extrabold text-7xl">LMCC Console</h1>
+            <Clock />
+        </div>
+    );
 }
 
 export default function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Hello />} />
-      </Routes>
-    </Router>
-  );
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Hello />} />
+            </Routes>
+        </Router>
+    );
 }
