d80221cc0562e046f423ad7c3dbfa0864f0dd9e7
abhi-arya1 <abhia9167@gmail.com>
2024-01-03 12:22:59 -0800

    remove unused linter

--- components/rover/rover_live_view.tsx
+++ components/rover/rover_live_view.tsx
@@ -5,34 +5,27 @@
  * https://docs.google.com/document/d/1wiDTSK4uR1we2z8LcobuU8MG8j1gtWkV8PsYyITrCy4/
  */
 
-/* eslint-disable react/require-default-props */
-import ReactPlayer from 'react-player';
+import ReactPlayer from "react-player";
 
 interface EVALiveViewProps {
-    url: string;
-    volume?: number;
+  url: string;
+  volume?: number;
 }
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 function RoverLiveView({ url, volume = 0.5 }: EVALiveViewProps) {
-    const playerStyle = {
-        borderBottomLeftRadius: '12px',
-        borderBottomRightRadius: '12px',
-        borderTopRightRadius: '12px',
-        borderTopLeftRadius: '12px',
-        overflow: 'hidden',
-    };
+  const playerStyle = {
+    borderBottomLeftRadius: "12px",
+    borderBottomRightRadius: "12px",
+    borderTopRightRadius: "12px",
+    borderTopLeftRadius: "12px",
+    overflow: "hidden",
+  };
 
-    return (
-        <div>
-            <ReactPlayer
-                url={url}
-                playing
-                volume={volume}
-                style={playerStyle}
-            />
-        </div>
-    );
+  return (
+    <div>
+      <ReactPlayer url={url} playing volume={volume} style={playerStyle} />
+    </div>
+  );
 }
 
 export default RoverLiveView;
