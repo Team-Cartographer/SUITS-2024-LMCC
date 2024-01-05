fde3bbedf2feb28d5febc8909ca52f2be3477dcc
abhi-arya1 <abhia9167@gmail.com>
2024-01-05 13:18:44 -0800

    clean batch files

--- server/start_pwsh.bat
+++ server/start_pwsh.bat
@@ -1,25 +1,25 @@
-# Ensure any errors stop the script
+REM Ensure any errors stop the script
 $ErrorActionPreference = "Stop"
 
-# Change directory to the location of the script
+REM Change directory to the location of the script
 cd $PSScriptRoot
 
-# Create a Python virtual environment
+REM Create a Python virtual environment
 python -m venv venv
 
-# Activate the virtual environment
+REM Activate the virtual environment
 . .\venv\Scripts\Activate.ps1
 
-# Upgrade pip
+REM Upgrade pip
 pip install --upgrade pip
 
-# Install dependencies
+REM Install dependencies
 pip install flask
 pip install flask-cors
 pip install requests
 
-# Execute the server script
+REM Execute the server script
 python server.py
 
-# Deactivate the virtual environment
+REM Deactivate the virtual environment
 deactivate
