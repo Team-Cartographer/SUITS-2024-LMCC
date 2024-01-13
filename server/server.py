43bc4c3d01b2149f84365940b458993718c16919
Abhi Arya <abhia9167@gmail.com>
2024-01-13 14:54:59 -0800

    update routes for api

--- server/server.py
+++ server/server.py
@@ -6,7 +6,7 @@ from flask_cors import CORS
 from routes.tss import tss
 from routes.tests import tests
 from routes.mission import mission
-from routes.api import api
+from api import api
 
 # server app instance
 app = Flask(__name__)
