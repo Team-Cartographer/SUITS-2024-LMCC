8032a9958cad84d2a0aa3f8a22e1096b541e7d92
Hrishikesh Srihari <rishisrihari9@gmail.com>
2024-01-13 18:51:27 -0800

    created Node class to represent each weighted node in the grid

--- server/functions/astar.py
+++ server/functions/astar.py
@@ -1,6 +1,24 @@
-# implementation of A* for JSC Rockyard will go here:
+import numpy as np
 
-# for now, this is a temp file
+
+class Node:
+    def __init__(self, parent=None, position=None):
+        self.parent = parent
+        self.position = position
+        self.cost_to_reach = 0
+        self.total_cost = 0
+        self.heuristic_cost = 0
+        
+    def __eq__(self, compare):
+        return self.position == compare.position
+    
+    def __lt__(self, compare):
+        return self.cost_to_reach < compare.cost_to_reach
+        
+
+
+
+
 
 if __name__ == "__main__":
     print("A* goes here")
