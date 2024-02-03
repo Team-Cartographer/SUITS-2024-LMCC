898f51865f61f8c8bb1417f66dcf995e0b37cf3c
Isaiah <148330546+DatsIsaiah@users.noreply.github.com>
2024-02-03 13:04:54 -0800

    Added author section back

--- client/app/screen_two/page.tsx
+++ client/app/screen_two/page.tsx
@@ -1,5 +1,11 @@
 // ./app/screen_two/page.tsx
 // Code notes by Isaiah: https://docs.google.com/document/d/17xVcVvvVmFHU4XP4ukxu7YCvT04KC2miXijcyR0V_II/edit?usp=sharing
+/**
+ * @author @abhi-arya1
+ * @author @datsisaiah 
+ * @function TelemetryPage
+ * @fileoverview Coming once all "TODO:" are complete
+ */
 'use client';
 
 import React from 'react';
