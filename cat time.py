0c497b0b7177fcbc4aa99d4121cf6cf9252e4030
Sebastian Reyes <136374529+Sebastianr8243@users.noreply.github.com>
2024-02-04 14:36:49 -0800

    hi

--- cat time.py
+++ cat time.py
@@ -1,50 +1,30 @@
-import time
+import tkinter as tk
 
-dancing_cats = [
-    [
-        r'''
-   /\_/\  
-  / o o \ 
- (   "   )
-  \~(*)~/
-''',
-        r'''
-   /\_/\  
-  / o o \ 
- (   "   )
-  \~(*)~/
-'''
-    ],
-    [
-        r'''
- /\_/\  
-( o.o ) 
- > ^ <
-''',
-        r'''
- /\_/\  
-( -.- ) 
- > ^ <
-'''
-  [
-        r'''
-   /\_/\  
-  / o o \ 
- (   "   )
-  \~(*)~/
-''',
-    ]
-]
+def remove_pin(event):
+    if event.widget.winfo_class() == 'Pin':
+        event.widget.destroy()
+        update_geojson()
 
+def update_geojson():
+    # this function to update the GeoJSON data
+    # and send it to the server to remove the pin permanently
+    pass  # Add your implementation here
 
-# Function to clear the screen
-def clear_screen():
-    print("\033[H\033[J", end='')
+def update_pin_positions():
+    # put this function to adjust the positions of pins
+    # based on the new dimensions of the map or interface
+    pass  # Add your implementation here
 
-# Main loop to display the dancing cat frames
-while True:
-    for cat_frames in dancing_cats:
-        for frame in cat_frames:
-            clear_screen()
-            print(frame)
-            time.sleep(0.5)  # Adjust sleep time to change animation speed
+root = tk.Tk()
+root.geometry("800x600")
+
+canvas = tk.Canvas(root, bg="white")
+canvas.pack(fill=tk.BOTH, expand=True)
+
+canvas.bind("<Button-1>", remove_pin)
+root.bind("<Configure>", lambda event: update_pin_positions())
+
+# Create a pin 
+pin = canvas.create_oval(100, 100, 120, 120, fill="red", outline="black", tags="Pin")
+
+root.mainloop()
