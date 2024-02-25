c6242f0c356774143736742de589fd27bdbb88f5
areich128 <alex.reich128@gmail.com>
2024-02-25 15:04:04 -0700

    worked on adding data table to page 2, and created easier access method for API call

--- client/components/hmd_link/geo_sampling.tsx
+++ client/components/hmd_link/geo_sampling.tsx
@@ -8,6 +8,8 @@
 import { fetchWithParams, fetchWithoutParams } from "@/api/fetchServer";
 import { useState } from "react";
 import { useEffect } from "react";
+import { DataTable } from "@/components/ui/data_table";
+import { ColumnDef } from "@tanstack/react-table"
 
 interface SpecData {
     Al2O3: number;
@@ -36,23 +38,46 @@ interface SpecData {
     spec: Spec;
   }
 
+let EVA1SpecItem: SpecItem | null;
+let EVA2SpecItem: SpecItem | null;
+
 const GeoSampler = () => {
   const [idValue, setIdValue] = useState(0);
+  const [EVA1SpecItem, setEVA1SpecItem] = useState<SpecItem | null>(null);
+  const [EVA2SpecItem, setEVA2SpecItem] = useState<SpecItem | null>(null);
+  
     
-  const fetchSampleID = async () => {
-    try {
-        const specData = await fetchWithoutParams<SpecData>("mission/spec");
-        console.log('API Response:', specData);
+    const fetchSampleID = async () => {
+        try {
+            const specData = await fetchWithoutParams<SpecData>("mission/spec");
+            console.log('API Response:', specData);
 
-        if (specData) {
-            console.log(specData.spec["eva1"].id);
-            setIdValue(specData.spec["eva1"].id);
+            if (specData) {
+                console.log(specData.spec["eva1"].id);
+                setIdValue(specData.spec["eva1"].id);
+
+                setEVA1SpecItem({
+                    data: specData.spec["eva1"].data,
+                    id: specData.spec["eva1"].id,
+                    name: specData.spec["eva1"].name
+                })
+                
+                setEVA2SpecItem({
+                    data: specData.spec["eva2"].data,
+                    id: specData.spec["eva2"].id,
+                    name: specData.spec["eva2"].name
+                })
+
+            }
+
+            
+
+        } catch (error) {
+            console.error('Error fetching data:', error);
         }
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-    }
     };
+
+    
 
     useEffect(() => {
         fetchSampleID();
@@ -63,9 +88,10 @@ const GeoSampler = () => {
 
   return (
     <div className="container mx-auto mt-8">
-        <h1 className="text-2xl font-bold mb-4">Sample ID</h1>
-        <p className={`text-3xl ${idValue !== 0 ? 'text-red-500' : 'text-green-500'}`}>
-            {idValue}
+        <h1 className="text-2xl font-bold mb-4">Geological Sampling</h1>
+        <h2 className="text-1xl font-bold mb-4">Sample Name, ID</h2>
+        <p className={`text-l ${idValue !== 0 ? 'text-green-500' : 'text-red-500'}`}>
+            {EVA1SpecItem?.name}, {EVA1SpecItem?.id}
         </p>
     </div>
   );
