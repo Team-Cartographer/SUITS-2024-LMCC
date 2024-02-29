e2ed9deea08f5eed3bd5f0394a616c47407998bc
Abhi Arya <abhia9167@gmail.com>
2024-02-28 23:05:36 -0800

    ready to pr, removed geo from page 2 now its in new pane

--- client/app/screen_two/page.tsx
+++ client/app/screen_two/page.tsx
@@ -1,22 +1,19 @@
 "use client";
 
+import Timers from "@/components/ui/timing";
+
 /**
  * @author @abhi-arya1 @ivanvuong @areich128
  * @function TelemetryPage
  * @fileoverview Coming once all "TODO:" are complete
  */
 
-import dynamic from 'next/dynamic'
- 
-const NoSSR_GeoSampler = dynamic(() => import('@/components/hmd_link/geo_sampling'), { ssr: false })
-
 
   function TelemetryPage() {
     return (
       <div className="h-full flex flex-row gap-x-4 items-center justify-center">
-          <NoSSR_GeoSampler />
+        Temp
       </div>
-      
     )
   }
 
