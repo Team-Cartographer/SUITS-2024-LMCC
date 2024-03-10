3de118eba256cb5ef656ffa65018870ab492768f
Ivan Vuong <vuongi2@uci.edu>
2024-03-09 21:10:19 -0800

    wassup

--- client/app/screen_two/page.tsx
+++ client/app/screen_two/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import Timers from "@/components/ui/timing";
+import PanicButton from "@/components/hmd_link/panic_button";
+import ContentManager from "@/components/general/content-manager";
 
 /**
  * @author @abhi-arya1 @ivanvuong @areich128
@@ -9,11 +11,23 @@ import Timers from "@/components/ui/timing";
  */
 
 
-  function TelemetryPage() {
-    return (
-      <div className="h-full flex flex-row gap-x-4 items-center justify-center">
-        Temp
+function TelemetryPage() {
+  return (
+    <div className="h-full flex flex-row gap-x-4">
+      <div className=" bg-slate-600 flex-grow items-center justify-center rounded-r-2xl p-2 pl-3">
+      <div className="flex flex-col items-start justify-center gap-y-10 ml-auto">
+      <span className="items-top-left justify-start text-6xl font-semibold p-4">
+        Rover Pilot Interface
+      </span>
       </div>
+    </div>
+    <ContentManager />
+    <div className="absolute top-0 right-0 p-1 h-full flex flex-row gap-x-1 justify-start text-3xl font-bold"> 
+      <Timers />
+    </div>
+    <div className="absolute top-10 left-[900px] p-1 h-full flex flex-row gap-x-1 justify-start text-3xl font-bold"> <PanicButton /></div>
+    </div>
+
     )
   }
 
