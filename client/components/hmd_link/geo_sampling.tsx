eb5f3a90f73285b29b6fc98f5ebadb86764d789a
areich128 <alex.reich128@gmail.com>
2024-03-17 13:20:38 -0600

    added geo data storage capability. formatting still needs work

--- client/components/hmd_link/geo_sampling.tsx
+++ client/components/hmd_link/geo_sampling.tsx
@@ -33,13 +33,19 @@ import { SpecItem } from "@/hooks/types"
 
 
 const GeoSampler = () => {
-  const networkProvider = useNetwork();
-  const [id1Value, setId1Value] = useState(0);
-  const [id2Value, setId2Value] = useState(0);
-  const [EVA1SpecItem, setEVA1SpecItem] = useState<SpecItem | null>(null);
-  const [EVA2SpecItem, setEVA2SpecItem] = useState<SpecItem | null>(null);
-  const [switchState, setSwitchState] = useState<boolean>(false);
-  const [todoItems, setTodoItems] = useState<[string, string][]>();
+    const [Spec1Items, setSpec1Items] = useState<SpecItem[]>([]);
+    const [Spec2Items, setSpec2Items] = useState<SpecItem[]>([]);
+    const [currentId1, setCurrentId1] = useState<number>(0);
+    const [currentId2, setCurrentId2] = useState<number>(0);
+    const networkProvider = useNetwork();
+    const [id1Value, setId1Value] = useState(0);
+    const [id2Value, setId2Value] = useState(0);
+    const [id1Name, setId1Name] = useState('');
+    const [id2Name, setId2Name] = useState('');
+    const [EVA1SpecItem, setEVA1SpecItem] = useState<SpecItem | null>(null);
+    const [EVA2SpecItem, setEVA2SpecItem] = useState<SpecItem | null>(null);
+    const [switchState, setSwitchState] = useState<boolean>(false);
+    const [todoItems, setTodoItems] = useState<[string, string][]>();
     
     const handleSwitchChange = () => {
         setSwitchState((prevChecked) => !prevChecked);
@@ -52,6 +58,20 @@ const GeoSampler = () => {
             setId2Value(specData.eva2?.id || 0);
             setEVA1SpecItem(specData.eva1);
             setEVA2SpecItem(specData.eva2);
+
+            if (specData.eva1?.id && specData.eva1.id !== id1Value) {
+                setId1Value(specData.eva1.id); 
+                setEVA1SpecItem(specData.eva1);
+                setCurrentId1(specData.eva1.id);
+            }
+            if (specData.eva2?.id && specData.eva2.id !== id2Value) {
+                setId2Value(specData.eva2.id); 
+                setEVA2SpecItem(specData.eva2);
+                setCurrentId2(specData.eva2.id);
+            }
+            //for the time being, only changing eva1 as a proof of concept. 
+            //REMEMBER TO CHANGE FOR EVA2 EVENTUALLY
+
         } catch (error) {
             console.error('Error fetching data:', error);
         }
@@ -99,6 +119,28 @@ const GeoSampler = () => {
         return () => clearInterval(intervalID);
     });
 
+    useEffect(() => {
+        if ((currentId1 !== 0) && (EVA1SpecItem?.data) && (EVA1SpecItem?.name)){
+            const newSpecItem: SpecItem = {
+                data: EVA1SpecItem?.data,
+                id: EVA1SpecItem?.id,
+                name: EVA1SpecItem?.name
+            }
+            setSpec1Items(prevSpecItem => [...prevSpecItem, newSpecItem])
+        }
+    }, [currentId1]);
+
+    useEffect(() => {
+        if ((currentId2 !== 0) && (EVA2SpecItem?.data) && (EVA2SpecItem?.name)){
+            const newSpecItem: SpecItem = {
+                data: EVA2SpecItem?.data,
+                id: EVA2SpecItem?.id,
+                name: EVA2SpecItem?.name
+            }
+            setSpec2Items(prevSpecItem => [...prevSpecItem, newSpecItem])
+        }
+    }, [currentId2]);
+
     const chart1Data = EVA1SpecItem?[['Element', 'Value'], ...Object.entries(EVA1SpecItem.data)]
         : [];
 
@@ -124,9 +166,8 @@ const GeoSampler = () => {
 
 
   return (
-    <div className="container mx-auto mt-8">
-        <h1  style={{ textAlign: 'center' }} className="text-2xl font-bold mb-4">Geological Sampling </h1>
-        <p className={`text-l ${id1Value !== 0 ? 'text-green-500' : 'text-red-500'}`}>
+    <div style={{ height: '620px', width: '800px' }} className="flex flex-col container mx-auto mt-8">
+        <p style={{ marginTop: '-50px' }} className={`text-l ${id1Value !== 0 ? 'text-green-500' : 'text-red-500'}`}>
             EVA 1 Data:
         </p>
         <Table>
@@ -139,35 +180,30 @@ const GeoSampler = () => {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                    <TableCell className="w-[50px]">{EVA1SpecItem?.name}</TableCell>
-                    <TableCell className="w-[50px]">{EVA1SpecItem?.id}</TableCell>
+                {Spec1Items.map((specItem, index) => (
+                <TableRow key={index}>
+                    <TableCell className="w-[50px]">{specItem.name}</TableCell>
+                    <TableCell className="w-[50px]">{specItem.id}</TableCell>
                     <TableCell className="w-[50px]">Collected</TableCell>
                     <Accordion type="multiple">
-                        <AccordionItem value="item-1">
+                        <AccordionItem value={`item-${index}`}>
                             <AccordionTrigger>
-                                Display {EVA1SpecItem?.name} data
+                                Display {specItem.name} data
                             </AccordionTrigger>
                             <AccordionContent>
                                 <Chart
                                     chartType="BarChart"
                                     width="100%"
                                     height="300px"
-                                    data={chart1Data}
-                                    options={options1}
+                                    data={[['Element', 'Value'], ...Object.entries(specItem.data)]}
+                                    options={{ title: specItem.name, legend: { position: 'none' }}}
                                     loader={<div>Loading Chart</div>}
                                 />
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
-                {/*
-                    <TableCell className="font-medium">{EVA1SpecItem?.name}</TableCell>
-                    <TableCell>{EVA1SpecItem?.id}</TableCell>
-                    <TableCell>Collected</TableCell>
-                    <TableCell className="text-right"><Switch />
-                    </TableCell>
-                    */}
                 </TableRow>
+                ))}
             </TableBody>
         </Table>
         <div style={{ height: '60px' }}></div>
@@ -184,35 +220,30 @@ const GeoSampler = () => {
                 </TableRow>
             </TableHeader>
             <TableBody>
-                <TableRow>
-                    <TableCell className="w-[50px]">{EVA2SpecItem?.name}</TableCell>
-                    <TableCell className="w-[50px]">{EVA2SpecItem?.id}</TableCell>
+                {Spec2Items.map((specItem, index) => (
+                <TableRow key={index}>
+                    <TableCell className="w-[50px]">{specItem.name}</TableCell>
+                    <TableCell className="w-[50px]">{specItem.id}</TableCell>
                     <TableCell className="w-[50px]">Collected</TableCell>
                     <Accordion type="multiple">
-                        <AccordionItem value="item-1">
+                        <AccordionItem value={`item-${index}`}>
                             <AccordionTrigger>
-                                Display {EVA2SpecItem?.name} data
+                                Display {specItem.name} data
                             </AccordionTrigger>
                             <AccordionContent>
                                 <Chart
                                     chartType="BarChart"
                                     width="100%"
                                     height="300px"
-                                    data={chart2Data}
-                                    options={options2}
+                                    data={[['Element', 'Value'], ...Object.entries(specItem.data)]}
+                                    options={{ title: specItem.name, legend: { position: 'none' }}}
                                     loader={<div>Loading Chart</div>}
                                 />
                             </AccordionContent>
                         </AccordionItem>
                     </Accordion>
-                {/*
-                    <TableCell className="font-medium">{EVA1SpecItem?.name}</TableCell>
-                    <TableCell>{EVA1SpecItem?.id}</TableCell>
-                    <TableCell>Collected</TableCell>
-                    <TableCell className="text-right"><Switch />
-                    </TableCell>
-                    */}
                 </TableRow>
+                ))}
             </TableBody>
         </Table>
     </div>
@@ -225,3 +256,13 @@ export default GeoSampler;
 <TableCell className="text-right"><Switch checked={field.value} onCheckedChange={field.onChange} />
 </TableCell>
 */}
+
+{/*
+    <TableCell className="font-medium">{EVA1SpecItem?.name}</TableCell>
+    <TableCell>{EVA1SpecItem?.id}</TableCell>
+    <TableCell>Collected</TableCell>
+    <TableCell className="text-right"><Switch />
+    </TableCell>
+    */}
+
+    // <h1 style={{ textAlign: 'center'  }} className="text-2xl font-bold mb-4">Geological Sampling </h1>
