77db9e37baf06660a0ed4329f80872bfb648107d
Abhi Arya <abhia9167@gmail.com>
2024-03-25 18:02:36 -0700

    fix(camera_feed): fixed camera feed to use hololens api correctly

--- client/components/hmd_link/camera_feed.tsx
+++ client/components/hmd_link/camera_feed.tsx
@@ -6,47 +6,6 @@
  * The camera feed component for the LMCC console. 
  */
 
-import React, { useState, useEffect } from 'react';
-import lmcc_config from "@/lmcc_config.json"
-import BiometricTelemetry from './biometric_data';
-
-
-const fetchVideoWithoutParams = async (path: string): Promise<Response | undefined> => {
-    try { // FIXME: PR will be approved w/ this, but this path will need to be changed once we get HoloLenses
-        const response = await fetch(`${lmcc_config.lmcc_url}/${path}`, {
-            method: 'GET',  
-        });
-        if (response.ok) {
-            return response;
-        } else {
-            console.error('Failed to fetch video from the server');
-        }
-    } catch (error) {
-        console.error('Error while fetching video:', error);
-    }
-    return undefined;
-}
-
-
-const fetchVideo = async () => {
-    try {
-        const response = await fetchVideoWithoutParams('/api/holographic/stream/live_high.mp4?holo=true&pv=true&mic=false&loopback=true&RenderFromCamera=true');
-        if (!response) {
-            throw new Error('Failure')
-        }
-        const videoBlob = await response.blob();
-        if (videoBlob) {
-            const videoObjectURL = URL.createObjectURL(videoBlob);
-            return videoObjectURL;
-        } else {
-            throw new Error('Video blob is undefined');
-        }
-    } catch (err) {
-        const error = err as Error;
-        console.error('Error fetching video:', error);
-        return undefined
-    };
-};
 
 interface CameraFeedProps {
     className?: string
@@ -54,42 +13,13 @@ interface CameraFeedProps {
 
 
 export function CameraFeed({ className }: CameraFeedProps) {
-    const [videoUrl, setVideoUrl] = useState<string | undefined>(undefined);
-    useEffect(() => {
-        const fetchURL = async () => {
-            try {
-                const url = await fetchVideo();
-                setVideoUrl(url);
-            } catch (error) {
-                console.error('Error fetching video:', error);
-            }
-        };
-        fetchURL();
-    }, []);
-        return (
-            <div className={`justify-center items-center ${className}`}>
-            <video controls width="1400">
-                <source src={videoUrl} type="video/mp4" />
-                Your browser does not support the video tag.
-            </video>
-            </div>
-        );
-}
-
-export function TempYoutubeVideo() {
-return (
-    <div className="justify-center items-center">
-        <div>
-            <iframe
-                width="1236" 
-                height="695" 
-                src="https://www.youtube.com/embed/jfKfPfyJRdk" 
-                title="lofi hip hop radio 📚 - beats to relax/study to" 
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                allowFullScreen={true}
-            ></iframe>
+    return (
+        <div className={`justify-center items-center ${className}`}>
+        <video controls width="1400" autoPlay={true} loop={true}>
+            <source src="https://192.168.0.19/api/holographic/stream/live.mp4" type="video/mp4" />
+            Your browser does not support the video tag.
+        </video>
         </div>
-    </div>
-);
+    );
 }
 
