023e95085e6f5bac3dd12bc86f739b5283e892fe
Abhi Arya <abhia9167@gmail.com>
2024-03-28 15:56:59 -0700

    fix(camera_feed): added padding for cam feeds

--- client/components/hmd_link/camera_feed.tsx
+++ client/components/hmd_link/camera_feed.tsx
@@ -15,7 +15,7 @@ interface CameraFeedProps {
 
 export function CameraFeed({ ip, className }: CameraFeedProps) {
     return (
-        <div className={`justify-center items-center ${className}`}>
+        <div className={`justify-center items-center pb-6 ${className}`}>
         <video controls width="1400" autoPlay={true} loop={true}>
             <source src={`https://${ip}/api/holographic/stream/live.mp4`} type="video/mp4" />
             Your browser does not support the video tag.
