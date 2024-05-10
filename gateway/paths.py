a6a537ff5ef376cbabcc34146aa915ae3bba2817
Abhi Arya <abhigyaa@uci.edu>
2024-05-09 22:56:34 -0700

    forgot these files in the last commit, whoops

--- gateway/paths.py
+++ gateway/paths.py
@@ -1,4 +1,5 @@
-from pathlib import Path 
+from pathlib import Path
+
 
 PARENT = Path(__file__).parent
 DATA_D = PARENT / 'data'
@@ -12,3 +13,5 @@ WARNING_PATH = DATA_D / 'warning.json'
 TIFF_PATH = PARENT / 'services' / 'rockyard.tif'
 PNG_PATH = PARENT / 'services' / 'rockyardimg.png'
 GEODATA_PATH = PARENT / 'services' / 'geodata.npy'
+SPATIAL_PATH = DATA_D / 'SpatialMapping.obj'
+SPATIAL_HEIGHTMAP_PATH = DATA_D / 'grid.npy' 
